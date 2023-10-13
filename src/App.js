3af0be187df56e13b02dbebb568d92bd58564a06
Shahir47 <shahir.glhd@gmail.com>
2023-10-13 12:30:14 -0500

    Update of 10/13/23

--- src/App.js
+++ src/App.js
@@ -19,11 +19,12 @@ const movie1 = {
     const [movies, setMovies] = useState([]);
     
     const searchMovies = async(title) =>{ //async, because it takes time to fetch the movies
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json(); // it is a data object
+        const response = await fetch(`${API_URL}&s=${title}`); //& is used to seperate parameters in url, maybe 's' is a parameter
+        const data = await response.json(); // This response object contains json data. .json() returns that json data as JS object.
+                            // We are also using "await" here because it takes time to parse the JSON data in the response object.
 
-        setMovies(data.Search);
-    } 
+        setMovies(data.Search); // This is a property of the JS object (which we got from the json data). It returns the search result requested using fetch.
+    }
 
     useEffect(() => {
         searchMovies('Spiderman');
@@ -31,7 +32,7 @@ const movie1 = {
 
     return (
         <div className="app">
-            <h1>FindMovies</h1>
+            <h1>Movies</h1>
 
             <div className="search">
                 <input
